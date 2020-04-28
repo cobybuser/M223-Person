3fd4a9fcfd93595800994199e7e69cb99bc3e988
Coby Buser <coby.buser@iwf.ch>
2020-04-28 12:38:21 +0200

    wokring on assignment

--- src/entity/Person.java
+++ src/entity/Person.java
@@ -1,13 +1,15 @@
 package entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Person {
-	
+
 	@Id
-	@
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String vorname;
@@ -15,7 +17,5 @@ public class Person {
 	private int plz;
 	private String ort;
 	private int telefonNr;
-	
-	
 
 }
